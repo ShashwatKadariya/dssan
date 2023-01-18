8cc75710e3aac0ed110d675ef2723e6a642177dd
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-01-18 13:26:33 +0545

    duplication validated

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -21,7 +21,7 @@ class BatchController extends Controller
     public function store(Request $request)
     {
         $validatedData = $this->validate($request, [
-            'batch' => 'required',
+            'batch' => 'required|unique:batches',
         ]);
         Batch::create($validatedData);
         return redirect()->route('batch.index')->with('success', 'Batch added successfully.');
