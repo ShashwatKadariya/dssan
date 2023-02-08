bc0925f1c2a90c5a21a4ebd92cf255650960693b
Tej Agrawal <tej.agrawal@deerwalk.edu.np>
2023-02-08 18:52:43 +0545

    alumni page image section

--- resources/views/users/alumni.blade.php
+++ resources/views/users/alumni.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.user_layout')
 
 @section('content')
+    {{-- image section --}}
+    <section>
+        <div class="relative float-right w-96 h-96 top-20 right-20">
+            <div class="absolute bg-cover h-36 w-36 top-18 rounded-full"
+                style="background-image: url('{{ asset('/images/images/gallery.png') }}')">
 
+            </div>
+            <div class="absolute bg-cover h-72 w-72 top-0 right-0 rounded-full"
+                style="background-image: url('{{ asset('/images/images/graduation-cropped.JPG') }}')">
+
+            </div>
+            <div class="absolute bg-cover h-36 w-36 bottom-0 left-42 rounded-full"
+                style="background-image: url('{{ asset('/images/images/gallery.png') }}')">
+
+            </div>
+        </div>
+    </section>
 @endsection
