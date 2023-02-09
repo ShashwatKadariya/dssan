feaacc7cbbdcaca702881bbc0d867937601b450a
Tej Agrawal <tej.agrawal@deerwalk.edu.np>
2023-02-09 12:09:55 +0545

    alumni hero section frontend

--- resources/views/users/alumni.blade.php
+++ resources/views/users/alumni.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.user_layout')
 
 @section('content')
-    <section class="w-11/12 mx-auto mt-16">
+    <section class="w-11/12 mx-auto mt-20">
         {{-- image section --}}
         <div class="relative float-right w-96 h-96">
             <div class="absolute bg-cover h-36 w-36 top-18 rounded-full"
@@ -21,6 +21,20 @@
             <p class="w-1/2 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dignissimos fugit
                 voluptatibus aliquam unde nostrum! Fuga minima mollitia animi adipisci vitae, at quas sequi eos
                 reprehenderit! Ipsum, enim!</p>
+        </div>
+        <div class="my-8 flex gap-24">
+            <div>
+                <img src="{{ asset('images/icon/batch.png') }}"
+                    alt="link_to_twitter" class="h-14 object-contain aspect-3/2">
+                    <p class="text-center font-bold text-xl mt-2">21</p>
+                    <p class="text-center">Batches</p>
+                </div>
+            <div>
+                <img src="{{ asset('images/icon/graduate.png') }}"
+                    alt="link_to_twitter" class="h-14 object-contain aspect-3/2">
+                    <p class="text-center font-bold text-xl mt-2">54165</p>
+                    <p class="text-center">Graduates</p>
+            </div>
         </div>
     </section>
 @endsection
