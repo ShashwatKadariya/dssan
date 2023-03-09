3e7a1d0e4908908d22dcd35b386661b4309c4e80
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-03-09 14:51:08 +0545

    frontend fixes

--- resources/views/users/all_events.blade.php
+++ resources/views/users/all_events.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     {{-- events calender section --}}
-    <section class="w-11/12 mx-auto mt-18 xs:mt-20 mb-8 lg:mb-16">
+    <section class="w-11/12 lg:w-9/12 mx-auto mt-18 xs:mt-20 mb-8 lg:mb-16">
         <div data-aos="zoom-in-up" data-aos-duration="500">
             <p class="text-3xl font-semibold text-heading">EVENTS CALENDAR</p>
             <hr class="mt-2">
@@ -10,7 +10,7 @@
         <div class="mt-8 flex flex-col gap-8 lg:gap-16">
             @for ($i = 0; $i < 3; $i++)
                 <div data-aos="fade-up" data-aos-duration="800"
-                    class="flex flex-col-reverse sm:flex-row gap-2 justify-between items-center">
+                    class="flex flex-col-reverse sm:flex-row gap-2 justify-between items-center pl-4 border rounded-uihalf drop-shadow-sm">
                     <div class="flex gap-4">
                         <div class="flex flex-col gap-1 sm:gap-2 justify-evenly text-right pr-4 border-r-2">
                             <p class="font-medium">Mar</p>
