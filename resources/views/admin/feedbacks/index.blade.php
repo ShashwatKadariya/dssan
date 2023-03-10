e4f418e8a921bbb9a24775c4423546d134485ac8
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-03-10 12:53:40 +0545

    updated frontend

--- resources/views/admin/feedbacks/index.blade.php
+++ resources/views/admin/feedbacks/index.blade.php
@@ -11,11 +11,11 @@
             <table class="w-full text-sm">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr class="border">
+                        <th scope="col" class="p-3">Image</th>
                         <th scope="col" class="p-3">Name</th>
                         <th scope="col" class="p-3">Email address</th>
                         <th scope="col" class="p-3">Batch</th>
                         <th scope="col" class="p-3">Message</th>
-                        <th scope="col" class="p-3">Image</th>
                         <th scope="col" class="p-3">Display Status</th>
                         <th scope="col" colspan="2" class="p-3">Action</th>
                     </tr>
@@ -23,10 +23,6 @@
                 <tbody class="text-center">
                     @foreach ($feedbacks as $feedback)
                         <tr class="bg-white border hover:bg-gray-50">
-                            <td class="p-3">{{ $feedback->full_name }}</td>
-                            <td class="p-3">{{ $feedback->email }}</th>
-                            <td class="p-3">{{ $feedback->batch }}</td>
-                            <td class="p-3">{{ $feedback->message }}</td>
                             <td class="p-3">
                                 @if ($feedback->image)
                                     <div class="w-20 h-20 rounded-full bg-cover mx-auto"
@@ -36,6 +32,10 @@
                                         style="background-image: url({{ asset('images/default/default.png') }})"></div>
                                 @endif
                             </td>
+                            <td class="p-3">{{ $feedback->full_name }}</td>
+                            <td class="p-3">{{ $feedback->email }}</th>
+                            <td class="p-3">{{ $feedback->batch }}</td>
+                            <td class="p-3">{{ $feedback->message }}</td>
                             <td class="p-3">{{ $feedback->display_status }}</td>
                             <td class="p-3">
                                 <form id="submitForm_{{ $feedback->id }}"
