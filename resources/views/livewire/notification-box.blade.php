68a5c7510c1c7fd1e4d2b6a11804922e4d87fef5
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-03-25 11:48:32 +0545

    frontend fixes

--- resources/views/livewire/notification-box.blade.php
+++ resources/views/livewire/notification-box.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if ($message = Session::get('info'))
         <div id="notify" style="visibility: visible">
-            <div class="fixed top-0 bg-black w-full h-screen opacity-20"></div>
+            <div class="fixed top-0 bg-black w-full h-screen opacity-20 z-50"></div>
             <div class="notification z-50 bg-ui-white p-2 rounded-uihalf animate-appear" role="alert">
                 <div class="">
                     <img class="h-20 mx-auto" src="{{ asset('/images/icon/check.png') }}" alt="">
