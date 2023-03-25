0ebee827f28c08da27d545ea57f942d1dad061a3
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-03-25 16:02:49 +0545

    frontend fixes

--- resources/views/users/gallery.blade.php
+++ resources/views/users/gallery.blade.php
@@ -1,33 +1,27 @@
 @extends('layouts.user_layout')
 
 @section('content')
-    <section class="text-center text-dark-red mt-2">
-        <p class="text-lg font-medium leading-5">Welcome to the photo gallery section of</p>
-        <p class="text-lg font-semibold">Deerwalk Sifal School Alumni Association.</p>
-        <p class="text-2xl font-semibold pt-1">Life at DSS</p>
-    </section>
-
-    <section class="sm:grid sm:grid-cols-3">
-        <div class="bg-cover w-60 h-60 mx-auto my-8 text-white pt-32 pl-2"
-            style="background-image: url({{ asset('images/images/gallery.png') }})">
-            <p class="text-xl font-semibold">Book Reading Session</p>
-            <p class="text-xl font-semimedium">Dec 9-15, 2022</p>
-            <p class="text-lg font-medium pt-2">10 photos</p>
+    {{-- gallery section --}}
+    <section class="w-11/12 mx-auto mt-16 mb-8 lg:mb-16">
+        <div data-aos="zoom-in-up" data-aos-duration="500">
+            <p class="text-3xl font-semibold text-dwit-blue">LIFE AT DSS</p>
+            <hr class="mt-2">
         </div>
-        <div class="bg-cover w-60 h-60 mx-auto my-8"
-            style="background-image: url({{ asset('images/images/gallery.png') }})">
-        </div>
-        <div class="bg-cover w-60 h-60 mx-auto my-8"
-            style="background-image: url({{ asset('images/images/gallery.png') }})">
-        </div>
-        <div class="bg-cover w-60 h-60 mx-auto my-8"
-            style="background-image: url({{ asset('images/images/gallery.png') }})">
-        </div>
-        <div class="bg-cover w-60 h-60 mx-auto my-8"
-            style="background-image: url({{ asset('images/images/gallery.png') }})">
-        </div>
-        <div class="bg-cover w-60 h-60 mx-auto my-8"
-            style="background-image: url({{ asset('images/images/gallery.png') }})">
+        <div class="mt-8 grid sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
+            @for ($i = 0; $i < 9; $i++)
+                <a href="#" target="blank">
+                    <div data-aos="fade-up" data-aos-duration="800" class="relative border rounded-uihalf drop-shadow-sm">
+                        <div class="mx-auto aspect-3/2 object-contain bg-cover rounded-uihalf"
+                            style="background-image: url({{ asset('images/images/unnamed.jpg') }})">
+                        </div>
+                        <div class="absolute bg-black top-0 w-full h-full opacity-20"></div>
+                        <div class="absolute bottom-4 left-4 flex flex-col gap-2 text-white">
+                            <p class="text-xl font-medium drop-shadow-sm">Alumni Meet</p>
+                            <p class="font-medium drop-shadow-sm">10 photos</p>
+                        </div>
+                    </div>
+                </a>
+            @endfor
         </div>
     </section>
 @endsection
