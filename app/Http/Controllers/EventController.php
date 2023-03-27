f4cbb70bd12f2493d5b502f0d6d2a338e7137a7d
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-03-27 15:09:55 +0545

    updated orderby in index function

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,7 +11,7 @@ class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::orderBy('event_time', 'desc')->get();
+        $events = Event::orderBy('event_date', 'desc')->get();
         return view('admin.events.index', compact('events'));
     }
 
