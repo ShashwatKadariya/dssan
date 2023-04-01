9a198070243fc64be3b897fafdb1c35653604808
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-04-01 11:32:36 +0545

    fixed frontend and links

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,13 +1,13 @@
-<header class="z-10 fixed w-full top-0 bg-white/90 backdrop-blur-sm border-b sm:drop-shadow-sm">
+<header class="z-10 fixed w-full top-0 bg-white border-b drop-shadow-sm">
     <section class="mx-auto w-11/12 py-1.5 flex justify-between items-center">
-        <a href="/home">
+        <a href="/">
             <img src="{{ asset('images/logo/dssanblue.png') }}" alt="dssanlogo" class="max-w-[6rem]">
         </a>
         {{-- big screen navbar --}}
         <div>
             <button id="hamburger-button" class="text-black text-lg sm:hidden cursor-pointer">&#9776;</button>
             <nav class="hidden sm:block space-x-6 text-black font-semibold" aria-label="main">
-                <a href="/home" class="active:text-dwit-blue hover:text-dwit-blue delay-75">Home</a>
+                <a href="/" class="active:text-dwit-blue hover:text-dwit-blue delay-75">Home</a>
                 <a href="/gallery" class="active:text-dwit-blue hover:text-dwit-blue delay-75">Gallery</a>
                 <a href="/alumni" class="active:text-dwit-blue hover:text-dwit-blue delay-75">Alumni</a>
                 <a href="/news&events" class="active:text-dwit-blue hover:text-dwit-blue delay-75">News & Events</a>
@@ -16,15 +16,20 @@
     </section>
     {{-- mobile view navbar --}}
     <section id="mobile-menu"
-        class="absolute top-12 bg-ui-white backdrop-filter backdrop-blur bg-opacity-40 sm:hidden w-full hidden flex-col origin-top animate-open-menu">
+        class="absolute top-12 bg-ui-white sm:hidden w-full hidden flex-col origin-top animate-open-menu">
         <nav class="flex flex-col w-11/12 mx-auto pb-4 text-black" aria-label="mobile">
-            <a href="/home" class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">Home</a>
-            <a href="/gallery"
-                class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">Gallery</a>
-            <a href="/alumni"
-                class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">Alumni</a>
-            <a href="/news&events" class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">News
-                & Events</a>
+            <a href="/" class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">
+                Home
+            </a>
+            <a href="/gallery" class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">
+                Gallery
+            </a>
+            <a href="/alumni" class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">
+                Alumni
+            </a>
+            <a href="/news&events" class="py-1 active:text-dwit-blue hover:text-dwit-blue border-b border-black/70">
+                News & Events
+            </a>
         </nav>
     </section>
     {{-- current page nav-link highlighting script --}}
