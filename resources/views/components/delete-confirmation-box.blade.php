f5851e89d68c853f5888bdfb0a7b1d2aa0c42f4c
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-04-07 13:51:31 +0545

    fixed issues

--- resources/views/components/delete-confirmation-box.blade.php
+++ resources/views/components/delete-confirmation-box.blade.php
@@ -1,8 +1,8 @@
 <div id="deleteBox" class="hidden">
-    <div class="fixed top-0 bg-black w-full h-screen opacity-40"
-        onclick="document.getElementById('deleteBox').className = 'hidden'">
+    <div class="fixed top-0 bg-black w-full h-screen opacity-40">
     </div>
-    <div aria-hidden="true" class="absolute top-0 h-screen w-full flex justify-center items-center">
+    <div aria-hidden="true" class="absolute top-0 h-screen w-full flex justify-center items-center"
+        onclick="document.getElementById('deleteBox').className = 'hidden'">
         <div class="p-8 rounded-uihalf shadow-sm bg-gray-50 border">
             <p class="text-gray-600 text-lg">Are you sure you want to delete?</p>
             <p class="mb-1 text-sm text-gray-500">If you delete this row, you can't recover it.</p>
