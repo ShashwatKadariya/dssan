b43c55164006b7bd70345161e3d18e8b446b57b2
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-04-07 22:46:33 +0545

    implemented auth in components

--- resources/views/components/user-dropdown.blade.php
+++ resources/views/components/user-dropdown.blade.php
@@ -1,29 +1,26 @@
-<div id="dropdown-user" class="hidden">
-    <div class="fixed top-0 w-full h-screen bg-black/10" onclick="dropdownUser()">
-    </div>
-    <div
-        class="z-50 absolute top-16 right-5 border bg-white divide-y divide-gray-300 rounded-uihalf shadow origin-top animate-open-menu">
-        <div class="px-4 py-3">
-            <p class="text-sm font-medium">{{ Auth::user()->name }}</p>
-            <p class="text-sm font-medium">{{ Auth::user()->email }}</p>
+@auth
+    <div id="dropdown-user" class="hidden">
+        <div class="fixed top-0 w-full h-screen bg-black/10" onclick="dropdownUser()">
         </div>
-        <ul class="p-2">
-            <a href="#">
-                <li class="p-2 text-sm rounded-lg hover:bg-gray-100">
-                    Update Profile
+        <div
+            class="z-50 absolute top-16 right-5 border bg-white divide-y divide-gray-300 rounded-uihalf shadow origin-top animate-open-menu">
+            <div class="px-4 py-3">
+                <p class="text-sm font-medium">{{ Auth::user()->name }}</p>
+                <p class="text-sm font-medium">{{ Auth::user()->email }}</p>
+            </div>
+            <ul class="p-2">
+                <a href="/update-password">
+                    <li class="p-2 text-sm rounded-lg hover:bg-gray-100">
+                        Update Password
+                    </li>
+                </a>
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" class="p-2 text-sm rounded-lg hover:bg-gray-100">
+                        @csrf
+                        <button type="submit">Sign out</button>
+                    </form>
                 </li>
-            </a>
-            <a href="/update-password">
-                <li class="p-2 text-sm rounded-lg hover:bg-gray-100">
-                    Update Password
-                </li>
-            </a>
-            <li>
-                <form action="{{ route('logout') }}" method="POST" class="p-2 text-sm rounded-lg hover:bg-gray-100">
-                    @csrf
-                    <button type="submit">Sign out</button>
-                </form>
-            </li>
-        </ul>
+            </ul>
+        </div>
     </div>
-</div>
+@endauth
