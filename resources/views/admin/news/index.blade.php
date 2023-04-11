6828d10d8982830d61c28353ddd2399d86a470a8
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-04-11 15:19:00 +0545

    updated frontend

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -1,57 +1,56 @@
 @extends('layouts.admin_layout')
 
 @section('content')
-    <div class="p-4">
-        <a class="back-btn" href="/dashboard">Back</a>
-        <div class="flex flex-row justify-between px-2 py-4">
-            <div>
-                <p class="text-xl font-semibold">News</p>
-                <p class="text-sm">A list of news article by DSS Alumni.</p>
-            </div>
-            <form action="{{ route('news.create') }}">
-                <button type="submit" class="create-btn">Add News</button>
-            </form>
+    <div class="flex flex-row justify-between my-4">
+        <div>
+            <p class="text-xl font-semibold">News</p>
+            <p class="text-sm">A list of news article by DSS Alumni.</p>
         </div>
-        <div class="relative overflow-x-auto">
-            <table class="w-full text-sm">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                    <tr class="border">
-                        <th scope="col" class="p-3">News Banner</th>
-                        <th scope="col" class="p-3">Headline</th>
-                        <th scope="col" class="p-3">Tagline</th>
-                        <th scope="col" class="p-3">Reported By</th>
-                        <th scope="col" class="p-3">Release Date</th>
-                        <th scope="col" colspan="3" class="p-3">Action</th>
+        <form action="{{ route('news.create') }}">
+            <button type="submit" class="button">Add News</button>
+        </form>
+    </div>
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr class="border">
+                    <th scope="col" class="p-3">News Banner</th>
+                    <th scope="col" class="p-3">Headline</th>
+                    <th scope="col" class="p-3">Tagline</th>
+                    <th scope="col" class="p-3">Reported By</th>
+                    <th scope="col" class="p-3">Release Date</th>
+                    <th scope="col" colspan="3" class="p-3">Action</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                @foreach ($news as $single_news)
+                    <tr class="bg-white border hover:bg-gray-50">
+                        <td class="p-3">
+                            <div class="h-20 aspect-square object-contain bg-cover mx-auto"
+                                style="background-image: url({{ asset('/storage/' . $single_news->image) }})"></div>
+                        </td>
+                        <td class="p-3">{{ $single_news->headline }}</td>
+                        <td class="p-3">{{ $single_news->tagline }}</th>
+                        <td class="p-3">{{ $single_news->reported_by }}</td>
+                        <td class="p-3">{{ $single_news->release_date->format('d M, Y') }}</td>
+                        <td class="p-3">
+                            <form id="submitForm_{{ $single_news->id }}"
+                                action="{{ route('news.destroy', $single_news->id) }}" method="POST">
+                                <a class="mx-2 font-medium text-green-600 hover:underline"
+                                    href="{{ route('news.show', $single_news->id) }}">View</a>
+                                <br>
+                                <a class="mx-2 font-medium text-blue-600 hover:underline"
+                                    href="{{ route('news.edit', $single_news->id) }}">Edit</a>
+                                <br>
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" class="mx-2 font-medium text-red-600 hover:underline"
+                                    onclick="deleteBox({{ $single_news->id }})">Delete</button>
+                            </form>
+                        </td>
                     </tr>
-                </thead>
-                <tbody class="text-center">
-                    @foreach ($news as $single_news)
-                        <tr class="bg-white border hover:bg-gray-50">
-                            <td class="p-3">
-                                <div class="h-20 aspect-square object-contain bg-cover mx-auto"
-                                    style="background-image: url({{ asset('/storage/' . $single_news->image) }})"></div>
-                            </td>
-                            <td class="p-3">{{ $single_news->headline }}</td>
-                            <td class="p-3">{{ $single_news->tagline }}</th>
-                            <td class="p-3">{{ $single_news->reported_by }}</td>
-                            <td class="p-3">{{ $single_news->release_date->format('d M, Y') }}</td>
-                            <td class="p-3">
-                                <form id="submitForm_{{ $single_news->id }}"
-                                    action="{{ route('news.destroy', $single_news->id) }}" method="POST">
-                                    <a class="mx-2 font-medium text-green-600 hover:underline"
-                                        href="{{ route('news.show', $single_news->id) }}">View</a>
-                                    <a class="mx-2 font-medium text-blue-600 hover:underline"
-                                        href="{{ route('news.edit', $single_news->id) }}">Edit</a>
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button" class="mx-2 font-medium text-red-600 hover:underline"
-                                        onclick="deleteBox({{ $single_news->id }})">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
