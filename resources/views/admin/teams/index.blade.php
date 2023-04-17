2b6c7cf6f470ebe009ea6bf3369806785ca2a8ac
ag-tej <tej.agrawal@deerwalk.edu.np>
2023-04-17 12:35:26 +0545

    updated frontend

--- resources/views/admin/teams/index.blade.php
+++ resources/views/admin/teams/index.blade.php
@@ -1,61 +1,61 @@
 @extends('layouts.admin_layout')
 
 @section('content')
-    <div class="p-4">
-        <a class="back-btn" href="/dashboard">Back</a>
-        <div class="flex flex-row justify-between px-2 py-4">
-            <div>
-                <p class="text-xl font-semibold">Team</p>
-                <p class="text-sm">A list of current team members in DSSAN.</p>
-            </div>
-            <form action="{{ route('team.create') }}">
-                <button type="submit" class="create-btn">Add
-                    Member</button>
-            </form>
+    <div class="flex flex-row justify-between my-4">
+        <div>
+            <p class="text-xl font-semibold">Teams</p>
+            <p class="text-sm">A list of current team members in DSSAN.</p>
         </div>
-        <div class="relative overflow-x-auto">
-            <table class="w-full text-sm">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                    <tr class="border">
-                        <th scope="col" class="p-3">Image</th>
-                        <th scope="col" class="p-3">First Name</th>
-                        <th scope="col" class="p-3">Last Name</th>
-                        <th scope="col" class="p-3">Designation</th>
-                        <th scope="col" class="p-3">Position</th>
-                        <th scope="col" class="p-3">Statement</th>
-                        <th scope="col" class="p-3">LinkedIn url</th>
-                        <th scope="col" colspan="2" class="p-3">Action</th>
+        <form action="{{ route('team.create') }}">
+            <button type="submit" class="button">Add Member</button>
+        </form>
+    </div>
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr class="border">
+                    <th scope="col" class="p-3">Image</th>
+                    <th scope="col" class="p-3">First Name</th>
+                    <th scope="col" class="p-3">Last Name</th>
+                    <th scope="col" class="p-3">Designation</th>
+                    <th scope="col" class="p-3">Position</th>
+                    <th scope="col" class="p-3">Statement</th>
+                    <th scope="col" class="p-3">LinkedIn url</th>
+                    <th scope="col" colspan="2" class="p-3">Action</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                @foreach ($teams as $team)
+                    <tr class="bg-white border hover:bg-gray-50">
+                        <td class="p-3">
+                            <div class="w-20 h-20 rounded-full bg-cover mx-auto"
+                                style="background-image: url({{ asset('/storage/' . $team->image) }})"></div>
+                        </td>
+                        <td class="p-3">{{ $team->first_name }}</td>
+                        <td class="p-3">{{ $team->last_name }}</th>
+                        <td class="p-3">{{ $team->designation }}</td>
+                        <td class="p-3">{{ $team->position }}</td>
+                        <td class="p-3">{{ $team->statement }}</td>
+                        <td class="p-3">
+                            <a href="{{ $team->linkedin_url }}" target="blank">
+                                <img src="https://img.icons8.com/material-rounded/24/null/link--v1.png" alt="Link"
+                                    class="mx-auto">
+                            </a>
+                        </td>
+                        <td class="p-3">
+                            <form id="submitForm_{{ $team->id }}" action="{{ route('team.destroy', $team->id) }}"
+                                method="POST" class="flex flex-col gap-1">
+                                <a class="font-medium text-blue-600 hover:underline"
+                                    href="{{ route('team.edit', $team->id) }}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" class="font-medium text-red-600 hover:underline"
+                                    onclick="deleteBox({{ $team->id }})">Delete</button>
+                            </form>
+                        </td>
                     </tr>
-                </thead>
-                <tbody class="text-center">
-                    @foreach ($teams as $team)
-                        <tr class="bg-white border hover:bg-gray-50">
-                            <td class="p-3">
-                                <div class="w-20 h-20 rounded-full bg-cover mx-auto"
-                                    style="background-image: url({{ asset('/storage/' . $team->image) }})"></div>
-                            </td>
-                            <td class="p-3">{{ $team->first_name }}</td>
-                            <td class="p-3">{{ $team->last_name }}</th>
-                            <td class="p-3">{{ $team->designation }}</td>
-                            <td class="p-3">{{ $team->position }}</td>
-                            <td class="p-3">{{ $team->statement }}</td>
-                            <td class="p-3">{{ $team->linkedin_url }}</td>
-                            <td class="p-3">
-                                <form id="submitForm_{{ $team->id }}" action="{{ route('team.destroy', $team->id) }}"
-                                    method="POST">
-                                    <a class="font-medium text-blue-600 hover:underline pb-1"
-                                        href="{{ route('team.edit', $team->id) }}">Edit</a>
-                                    <br>
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button" class="font-medium text-red-600 hover:underline pt-1"
-                                        onclick="deleteBox({{ $team->id }})">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
